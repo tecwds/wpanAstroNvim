6156930d60ab1ec6a79473def07b6eb6e4b8a471
tecwds <tecwds@163.com>
2024-11-19 21:54:05 +0800

    mod: close auto formatter in save file

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -18,7 +18,8 @@ return {
     formatting = {
       -- control auto formatting on save
       format_on_save = {
-        enabled = true, -- enable or disable format on save globally
+        -- 不好用，全部关掉，手动 formatt, md 七煞我也
+        enabled = false, -- enable or disable format on save globally
         allow_filetypes = { -- enable format on save for specified filetypes only
           -- "go",
         },
